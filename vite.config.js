27e2da53e7a75d04931e371580d239e3ad552d8b
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-09 19:00:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/where-to/",
   plugins: [
     svelte(),
     webfontDownload(),
